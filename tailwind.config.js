fabee0cc59be3c240a0f85380605c64d9288905e
netraluis.castells@corusconsulting.com <netraluis.castells@corusconsulting.com>
2020-05-16 16:01:41 +0200

    container--about

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 module.exports = {
   theme: {
+    // colors: {
+    //   // primary: "#5c6ac4",
+    //   // secondary: "#f5f5f5",
+    //   // third: "#a52a2a",
+    //   // textTitle: "#525252",
+    //   // textColor: "#a2aeae",
+    //   // backgroundColor: "#f5f5dc",
+    //   // .btn-blue {
+    //   //   background-color: theme('colors.blue.500');
+    //   // }
+    // },
   },
   variants: {},
-  plugins: [
-    require('@tailwindcss/custom-forms'),
-  ]
-}
+  plugins: [require("@tailwindcss/custom-forms")],
+};
