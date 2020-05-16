5ef06fe8d8f009e9bbf1aced2e4340a09bc31f5b
netraluis.castells@corusconsulting.com <netraluis.castells@corusconsulting.com>
2020-05-16 15:25:48 +0200

    inicio

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+// import 'some-node-module';
+// import SomeModule from 'some-node-module';
+
+/**
+ * Write any other JavaScript below
+ */
+let $ = require("jquery");
++( function() {
+  console.log('Hello, UOC!');
+} )();
